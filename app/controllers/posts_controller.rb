b7934a05a80a0c4f97d2fac3df00343818a2cf3a
chiu <chiucode@gmail.com>
2015-05-25 01:05:58 +0000

    finished app message wall

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,18 +27,25 @@ class PostsController < ApplicationController
 end
 
 def edit
-    @post = Post.find(params[:id])
-  end
+  @post = Post.find(params[:id])
+end
 
-  def update
-    @post = Post.find(params[:id])
+def update
+  @post = Post.find(params[:id])
 
-    if @post.update_attributes(post_params)
-      redirect_to posts_path
-    else
-      render :edit
-    end
+  if @post.update_attributes(post_params)
+    redirect_to posts_path
+  else
+    render :edit
   end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to posts_path
+end
+
 
 protected
 
